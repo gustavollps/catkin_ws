fd83a5cc390c08bb254861f1e84299a6fdd7b231
Gustavo Leite Lopes <gustavollps@gmail.com>
2017-09-16 23:11:41 -0300

    New commit

--- src/tcc/control_loop_master/heading/src/libs/EKF_IMU.cpp
+++ src/tcc/control_loop_master/heading/src/libs/EKF_IMU.cpp
@@ -6,24 +6,26 @@
  */
 EKF_IMU::EKF_IMU(ros::NodeHandle* nh)
 {
-  mpu_.GetDescriptor();
-  mpu_.Gyro_SetRange2000();
-  mpu_.Accel_SetRange(ACCEL_SCALE_4G);
+  mpu_.GetDescriptor(); //get file descriptor to control the communication
+  mpu_.Gyro_SetRange2000(); //set gyroscope range to 2000 deegres/second
+  mpu_.Accel_SetRange(ACCEL_SCALE_4G); //set accelerometer range to 4G
 
-  nh_ = nh;
+  nh_ = nh; //pass argument to class node handle
 
-  float covariance_null[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  float covariance_null[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //initialize covariance for the sensors
 
-  imu_data_.header.stamp = ros::Time::now();
-  imu_data_.header.seq = 0;
-  imu_data_.header.frame_id = "base_footprint";
+  imu_data_.header.stamp = ros::Time::now();  //load time on imu message
+  imu_data_.header.seq = 0; //initialize message sequence
+  imu_data_.header.frame_id = "base_footprint"; //frame whose the sensor relationed
 
+  //initialize imu message data
   imu_data_.angular_velocity.x = 0;
   imu_data_.angular_velocity.y = 0;
 
   imu_data_.orientation.x = 0;
   imu_data_.orientation.y = 0;
 
+  //initialize imu message covariance
   for (int i = 0; i < 9; i++)
   {
     imu_data_.angular_velocity_covariance[i] = 0;
@@ -31,12 +33,16 @@ EKF_IMU::EKF_IMU(ros::NodeHandle* nh)
     imu_data_.orientation_covariance[i] = 0;
   }
 
+  //get parameters
   nh_->getParam("/Heading/ACCEL_OFFSET_X",accel_offset_x);
   nh_->getParam("/Heading/ACCEL_OFFSET_Y",accel_offset_y);
   nh_->getParam("/Heading/ACCEL_OFFSET_Z",accel_offset_z);
   nh_->getParam("/Heading/GYRO_OFFSET",gyro_offset_);
+
+  //initial giroscope angle
   gyro_angle_ = 0;
 
+//GYROSCOPE and ACCELEROMETER calibration routine. Not used after offset is fixed
 #ifdef CALIBRATION
   gyro_offset_ = 0;
   accel_offset_x = 0;
@@ -61,17 +67,25 @@ EKF_IMU::EKF_IMU(ros::NodeHandle* nh)
   ROS_INFO("OFFSETS: \nX: %f\nY: %f\nZ: %f",accel_offset_x,accel_offset_y,accel_offset_z);
 #endif
 
+  //boolean to control the publish rate
   publish_now_ = false;
 
+  //wiring PI library initialization (responsible for the i2c communication)
   wiringPiSetup();
 
-  imu_pub_ = nh_->advertise<sensor_msgs::Imu>("/imu_data", 10);
-  battery_level_ = nh_->advertise<std_msgs::Float32>("/battery_level",1);
-  input_level_ = nh_->advertise<std_msgs::Float32>("/input_level",1);
+  //publisher initialization
+  imu_pub_ = nh_->advertise<sensor_msgs::Imu>("/imu_data", 10); //imu msg publisher
+  battery_level_ = nh_->advertise<std_msgs::Float32>("/battery_level",1); //battery meter (no hardware implementation yet)
+  input_level_ = nh_->advertise<std_msgs::Float32>("/input_level",1); //5V regulator level (odroid power source)
 
+  //voltage publisher (1hz)
   levels_timer_ = nh_->createTimer(ros::Duration(1), &EKF_IMU::levels_timerCallBack, this);
+
+  //gyro and accel timer
   gyro_timer_ =
       nh_->createTimer(ros::Duration(0.01), &EKF_IMU::timerCallBack, this);  
+
+  //reset angle service
   zeroAngle_server_ =
       nh_->advertiseService("/ZeroAngle", &EKF_IMU::zeroangleCallBack, this);
 
@@ -131,6 +145,7 @@ void EKF_IMU::levels_timerCallBack(const ros::TimerEvent &event)
   msg.data = analogRead(1)*1.8/1024*11;
   input_level_.publish(msg);
 }
+
 /*
 
 X: 21.702000
@@ -155,6 +170,7 @@ Z: 1989.825000
 
 */
 
+//service to reset gyro angle
 bool EKF_IMU::zeroangleCallBack(tcc_msgs::ZeroAngle::Request& req,
                                 tcc_msgs::ZeroAngle::Response& res)
 {
--- src/trabalho_final/src/controlador.cpp
+++ src/trabalho_final/src/controlador.cpp
@@ -5,6 +5,6 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "Master_Node");
   trabalho::EmissorNode node(new ros::NodeHandle());
-  node.spin();
+  ros::spin();
   return 0;
 }
